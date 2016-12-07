e4648833fbf937522be38a83eab4798a634c4927
Nicolas <nicolas.aslantas@gmail.com>
2016-12-07 11:48:22 +0100

    Added Auth

--- booklocator/app.js
+++ booklocator/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var app = express();
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -6,8 +7,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
-var app = express();
 
+app.set("secret", "somerandomstring");
+var auth = require('./routes/auth')(app);
+var mongoose = require('mongoose');
+
+var port = process.env.PORT || 8080;
+mongoose.connect('mongodb://localhost/nicodb');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- booklocator/model/userDb.js
+++ booklocator/model/userDb.js
@@ -1,8 +1,4 @@
-// Load mongoose package
 var mongoose = require('mongoose');
-// Connect to MongoDB and create/use database called todoAppTest
-mongoose.connect('mongodb://localhost/nicodb');
-
 // Create a schema
 var userSchema = new mongoose.Schema({
     name: String,
