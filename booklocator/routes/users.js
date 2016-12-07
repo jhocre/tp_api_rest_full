ba19610e349af48590eb6652b893f36ed5b4439f
Guillaume <undogit@gmail.com>
2016-12-07 11:43:42 +0100

    mise en place /auth

--- booklocator/routes/users.js
+++ booklocator/routes/users.js
@@ -5,7 +5,7 @@ var userDB = require('../model/userDb');
 router.route("/").get(function (req, res) {
     var response = {};
     userDB.find({}, function (err, data) {
-        // Mongo command to fetch all data from collection.
+
         if (err) {
             response = {"error": true, "message": "Error fetching data"};
         } else {
@@ -17,13 +17,11 @@ router.route("/").get(function (req, res) {
 router.route("/").post(function (req, res) {
     var db = new userDB();
     var response = {};
-    // fetch email and password from REST request.
-    // Add strict validation when you use this in Production.
+
     db.name = req.body.name;
     db.password = req.body.password;
     db.save(function (err) {
-        // save() will run insert() command of MongoDB.
-        // it will add new data in collection.
+
         if (err) {
             response = {"error": true, "message": "Error adding data"};
         } else {
