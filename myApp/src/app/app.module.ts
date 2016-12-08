b4c80a90eb46453d869aa34f5c75063b996446d2
johann <johann.creneguy@gmail.com>
2016-12-08 17:17:05 +0100

    myapp finished

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { BookPage } from '../pages/book/book';
 import { CommandPage } from '../pages/command/command';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-import { ScanPage } from '../pages/scan/scan';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    BookPage,
     CommandPage,
-    ScanPage,
     TabsPage
   ],
   imports: [
@@ -23,9 +19,7 @@ import { ScanPage } from '../pages/scan/scan';
   entryComponents: [
     MyApp,
     HomePage,
-    BookPage,
     CommandPage,
-    ScanPage,
     TabsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -1,6 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {BarcodeScanner} from 'ionic-native';
 
-import { NavController } from 'ionic-angular';
+let user = "stephane";
+let psw = "root";
+
+let listRead = [];
+let listUnread = [];
+let manualIsbn = "";
+let connexion = "";
+
 
 @Component({
   selector: 'page-home',
@@ -9,11 +18,92 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   todo = {}
+
   logForm() {
     console.log(this.todo)
   }
+
   constructor(public navCtrl: NavController) {
 
   }
 
-}
+  connect() {
+    let userid = prompt('Enter your loggin :', '');
+    let password = prompt('Enter your password :', '');
+    alert('userid = ' + userid + '\npassword = ' + password);
+
+    if (userid === user && password === psw) {
+      alert("you are now connected");
+      connexion = "yes";
+      document.getElementById("connected").style.display="block";
+      document.getElementById("notConnected").style.display="none";
+    } else {
+      alert("Wrong loggin or password");
+    }
+  }
+
+  scan() {
+    if (connexion == "yes") {
+      BarcodeScanner.scan().then((barcodeData) => {
+
+        let isbn = barcodeData.text;
+
+        if (listRead.indexOf(isbn) > -1 || listUnread.indexOf(isbn) > -1) {
+          alert("Book allready added to list");
+        } else {
+          let r = confirm("Have you read it, "+isbn+" ?");
+          if (r == true) {
+            alert("Added to read list");
+            listRead.push(isbn);
+          } else {
+            alert("Added to unread list");
+            listUnread.push(isbn);
+          }
+        }
+
+
+      }, (err) => {
+        alert("You have an error :" + err + ", please enter your ISBN code manually");
+      })
+    } else {
+      alert("you need to be connected");
+    }
+  }
+
+    enter() {
+      if (connexion == "yes") {
+
+        manualIsbn = prompt("Enter the book ISBN please");
+        console.log(manualIsbn);
+
+        if (manualIsbn != "") {
+          if (listRead.indexOf(manualIsbn) > -1 || listUnread.indexOf(manualIsbn) > -1) {
+            alert("Book allready added to list");
+          } else {
+            let r = confirm("Have you read it, "+manualIsbn+" ?");
+            if (r == true) {
+              alert("Added to read list");
+              listRead.push(manualIsbn);
+            } else {
+              alert("Added to unread list");
+              listUnread.push(manualIsbn);
+            }
+          }
+        }else{
+          alert("Please enter an ISBN");
+        }
+      } else {
+        alert("you need to be connected");
+      }
+    }
+
+
+    check() {
+      if (connexion == "yes") {
+        alert("READ :\n" + listRead.join("\n") + "\nUNREAD :\n" + listUnread.join("\n"));
+      } else {
+        alert("you need to be connected");
+      }
+    }
+
+  }
